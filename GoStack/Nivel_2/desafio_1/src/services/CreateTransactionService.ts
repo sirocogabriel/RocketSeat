702246ba8469e3ceec8461b8ab613d80c18a8e94
sirocogabriel <sirocogabriel@gmail.com>
2020-05-06 08:28:07 -0300

    Desafio 1 Nivel 2 Completo

---
+++ GoStack/Nivel_2/desafio_1/src/services/CreateTransactionService.ts
@@ -0,0 +1,22 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
