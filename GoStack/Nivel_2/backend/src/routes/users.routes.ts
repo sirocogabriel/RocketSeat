d6cd85d3214a01a347f0622e8850100236f326ad
sirocogabriel <sirocogabriel@gmail.com>
2020-05-12 16:30:59 -0300

    files

--- GoStack/Nivel_2/backend/src/routes/users.routes.ts
+++ GoStack/Nivel_2/backend/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarSerice';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -34,7 +35,17 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({});
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
