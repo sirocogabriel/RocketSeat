d9e83f11e5f0bd9418a072dfe1ec48d819b980dc
sirocogabriel <sirocogabriel@gmail.com>
2020-05-12 17:03:02 -0300

    Nivel 2 Completo

--- GoStack/Nivel_2/backend/src/services/AuthenticateUserService.ts
+++ GoStack/Nivel_2/backend/src/services/AuthenticateUserService.ts
@@ -2,6 +2,9 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -21,13 +24,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorect email/password combination');
+      throw new AppError('Incorect email/password combination', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorect email/password combination');
+      throw new AppError('Incorect email/password combination', 401);
     }
 
     const { expiresIn, secret } = authConfig.jwt;
