ae590107f8435573bfa9f5e2103e9a42dbb92900
sirocogabriel <sirocogabriel@gmail.com>
2020-05-26 17:25:26 -0300

    Ok

--- GoStack/Nivel_3/frontend/src/App.tsx
+++ GoStack/Nivel_3/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+// import SignUp from './pages/SignUp'
 import GlobalStyle from './styles/global'
 
 import { AuthProvider } from './context/AuthContext'
